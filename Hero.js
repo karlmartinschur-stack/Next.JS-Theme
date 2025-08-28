279c18bf5b15fe66e94b602ab72a8e5fd5532dd2
karlmartinschur-stack <schur.karlmartin@gmail.com>
2025-08-28 02:56:51 +0200

    Create Hero.js

---
+++ Hero.js
@@ -0,0 +1,9 @@
+export default function Hero() {
+  return (
+    <section>
+      <h2>Webdesign aus Nürtingen und Raum Stuttgart</h2>
+      <p>WordPress Websiten, E-Mail-Konfigurationen & Migrationen – alles aus einer Hand.</p>
+      <img src="/images/hero.webp" alt="WordPress Webseiten" />
+    </section>
+  );
+}
