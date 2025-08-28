82ff807d62b69ec3a9c264c9ed164ce8203d3ccb
karlmartinschur-stack <schur.karlmartin@gmail.com>
2025-08-28 03:02:30 +0200

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ padding: '2rem', background: '#eee', textAlign: 'center' }}>
+      <p>&copy; {new Date().getFullYear()} Smartphone Reparaturen & Grafikdesign - Karl-Martin Schur</p>
+    </footer>
+  );
+}
