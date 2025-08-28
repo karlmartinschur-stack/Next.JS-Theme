c6fa67d914e723e82c3fea176da43ab1be2d7bef
karlmartinschur-stack <schur.karlmartin@gmail.com>
2025-08-28 02:59:30 +0200

    Create Services.js

---
+++ Services.js
@@ -0,0 +1,12 @@
+export default function Services() {
+  return (
+    <section>
+      <h2>Unsere Leistungen</h2>
+      <ul>
+        <li>Individuelles Webdesign & Webhosting</li>
+        <li>📧 E-Mail-Konfiguraion</li>
+        <li>📱 Webseiten-Migrationen</li>
+      </ul>
+    </section>
+  );
+}
