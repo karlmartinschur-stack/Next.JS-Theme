40d93b9d2908d80d3a132913d5de9817e558d90d
karlmartinschur-stack <schur.karlmartin@gmail.com>
2025-08-28 02:54:48 +0200

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header style={{ padding: '1rem 2rem', background: '#005a9c', color: '#fff' }}>
+      <h1>Karl-Martin Schur</h1>
+    </header>
+  );
+}
