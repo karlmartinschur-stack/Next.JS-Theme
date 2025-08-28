99e95142e7a48f9ca6354d5e49eb370fe8e0bbb1
karlmartinschur-stack <schur.karlmartin@gmail.com>
2025-08-28 03:01:01 +0200

    Create Contact.js

---
+++ Contact.js
@@ -0,0 +1,10 @@
+export default function Contact() {
+  return (
+    <section>
+      <h2>Kontakt</h2>
+      <p>📍 Nürtingen</p>
+      <p>📞 01578-2960567</p>
+      <p>✉️ info@karlmartinschur.de</p>
+    </section>
+  );
+}
